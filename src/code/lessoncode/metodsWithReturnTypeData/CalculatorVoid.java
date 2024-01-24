0d0795f973b685cb6b9f6d8effabede128a20195
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-24 22:06:12 +0100

    MultiCookerDemo.java

---
+++ src/code/lessoncode/metodsWithReturnTypeData/CalculatorVoid.java
@@ -0,0 +1,11 @@
+package code.lessoncode.metodsWithReturnTypeData;
+
+public class CalculatorVoid {
+
+    int result;
+
+    public void sum(int a, int b){
+        result = a + b;
+        System.out.println("Результат сложения x = " + a + " и у = " + b + " будет равен: " + result);
+    }
+}
---
+++ src/code/lessoncode/metodsWithReturnTypeData/CalculatorWithReturnResult.java
@@ -0,0 +1,13 @@
+package code.lessoncode.metodsWithReturnTypeData;
+
+public class CalculatorWithReturnResult {
+
+    public int sum(int a, int b){
+        int result = a + b;
+        return result;
+    }
+
+// добавить три метода которые бы возвращали результат вычитания, умножения и деления
+    // и использовать их
+
+}
