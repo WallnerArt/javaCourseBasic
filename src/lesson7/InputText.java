9c13142ce73e37864d576ece922c485875d1e193
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-25 10:04:02 +0100

    InputText.java

---
+++ src/lesson7/InputText.java
@@ -0,0 +1,13 @@
+package lesson7;
+
+import java.util.Scanner;
+
+public class InputText {
+    public String inputTextFromKeyboard(String message){
+        System.out.println(message);
+        Scanner scanner = new Scanner(System.in);
+        String userText = scanner.nextLine();
+
+        return userText;
+    }
+}
