6f6c9e12dfb3038aa2567d818f83a57f39ce25ff
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-25 21:55:00 +0100

    StudentManager.java

---
+++ src/students/StudentManager.java
@@ -0,0 +1,23 @@
+package students;
+
+import scanner.UserInput;
+
+import java.util.Scanner;
+
+public class StudentManager {
+
+
+    public Student createStudent() {
+
+        UserInput ui = new UserInput();
+
+        String lastName = ui.inputText("Фамилия: ");
+        String firstName = ui.inputText("Имя: ");
+        String studentCardNumber = ui.inputText("Номер студенческого: ");
+        String group = ui.inputText("Группа: ");
+
+        Student newStudent = new Student(lastName, firstName,studentCardNumber,group);
+
+        return newStudent;
+    }
+}
