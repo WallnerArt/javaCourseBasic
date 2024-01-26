ff6f3de2cd816264ffec0334c3d5f7a4edfcb906
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-27 00:54:31 +0100

    Phone.java

---
+++ src/lesson5/createObjectWithConstructor/Phone.java
@@ -0,0 +1,23 @@
+package lesson5.createObjectWithConstructor;
+
+public class Phone {
+
+    String modelName;
+
+    String simNumber;
+
+    public Phone() {
+    }
+
+    public Phone(String modelName) {
+        this.modelName = modelName;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "modelName='" + modelName + '\'' +
+                ", simNumber='" + simNumber + '\'' +
+                '}';
+    }
+}
