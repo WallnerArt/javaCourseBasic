317a6258af628baeb86b0f696f2119dc43631154
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-28 19:23:51 +0100

    Note.java

---
+++ src/lesson9/code/lessoncode/note/Note.java
@@ -0,0 +1,15 @@
+package lesson9.code.lessoncode.note;
+
+public class Note {
+    String number;
+    String content;
+
+    public Note(int number, String content) {
+        this.number = String.valueOf(number);
+        this.content = content;
+
+
+
+    }
+
+}
