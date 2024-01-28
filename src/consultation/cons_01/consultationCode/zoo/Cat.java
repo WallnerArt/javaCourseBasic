2c395fe0ad514c10285cb56e8ab187fa6bbd4371
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-28 19:05:54 +0100

    Cat.java

---
+++ src/consultation/cons_01/consultationCode/zoo/Cat.java
@@ -0,0 +1,17 @@
+package consultation.cons_01.consultationCode.zoo;
+
+public class Cat {
+    String name;
+
+    int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void greetings(){
+        System.out.println("Meow!");
+    }
+
+}
---
+++ src/lesson4/code/lessoncode/classMethods/Cat.java
@@ -0,0 +1,17 @@
+package lesson4.code.lessoncode.classMethods;
+
+public class Cat {
+    int age;
+
+    public void catMeow(){
+        System.out.println("Meow!!!");
+    }
+
+    public void catBirthday(String textMessage){
+        System.out.println(textMessage);
+        System.out.println("У моей кошки сегодня день рождения!");
+        System.out.println("Ей было " + age + " лет");
+        age = age + 1;
+        System.out.println("а теперь ей " + age);
+    }
+}
