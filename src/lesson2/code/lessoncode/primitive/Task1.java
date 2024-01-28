3631cfb57c2bbc387411fcc3c04b6356d4620f1b
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-28 19:43:59 +0100

    Task1.java

---
+++ src/lesson2/code/lessoncode/primitive/Task1.java
@@ -0,0 +1,12 @@
+package lesson2.code.lessoncode.primitive;
+
+public class Task1 {
+    public static void main(String[] args) {
+        byte varByte = 120;
+        int varInt = 1256256254;
+        double varDouble = 163.0;
+
+
+
+    }
+}
