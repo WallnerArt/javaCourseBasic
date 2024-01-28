1d5e6b60da88bba2e0aca59d904a2cd54db736e1
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-28 19:00:51 +0100

    Calculator.java

--- src/lesson8/code/lessoncode/calculatorTask5/Calculator.java
+++ src/lesson8/code/lessoncode/calculatorTask5/Calculator.java
@@ -1,7 +1,6 @@
-package calculatorTask5;
+package lesson8.code.lessoncode.calculatorTask5;
 
 public class Calculator {
-
     public double sum(double x, double y){
         double result = x + y;
         return result;
