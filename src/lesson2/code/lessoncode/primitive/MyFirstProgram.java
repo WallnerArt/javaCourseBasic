c9098c55de3d1fa4939ad8e08d0d7e0e3e58cced
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-28 19:23:37 +0100

    MyFirstProgram.java

---
+++ src/lesson2/code/lessoncode/primitive/MyFirstProgram.java
@@ -0,0 +1,11 @@
+package lesson2.code.lessoncode.primitive;
+public class MyFirstProgram {
+    // класс - это программа которую мы хотим чтобы java-машина выполнила
+
+    public static void main(String[] args) {
+        // main - это стандартный метод который java-машина
+        // понимает как точку входа (запуска) нашей программы
+
+        System.out.println("Hello, World!");
+    }
+}
