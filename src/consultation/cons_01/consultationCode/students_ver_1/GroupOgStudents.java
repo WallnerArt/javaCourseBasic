f02167cdbb3c41343b07f48f6d361e86c3bf8417
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-28 19:15:21 +0100

    GroupOgStudents.java

---
+++ src/consultation/cons_01/consultationCode/students_ver_1/GroupOgStudents.java
@@ -0,0 +1,18 @@
+package consultation.cons_01.consultationCode.students_ver_1;
+
+public class GroupOgStudents {
+    public static void main(String[] args) {
+        Student student1 = new Student("Petrova", "Olga", 4.5);
+        Student student2 = new Student("Smith", "John", 4.2);
+        Student student3 = new Student("Koval", "Ruslan", 4.7);
+        Student student4 = new Student("Smith", "Tatiana", 4.9);
+        Student student5 = new Student("Rubenshtain", "Serge", 4.4);
+
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student3);
+        System.out.println(student4);
+        System.out.println(student5);
+
+    }
+}
