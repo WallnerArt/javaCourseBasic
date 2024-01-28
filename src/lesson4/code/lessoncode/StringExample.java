df563b3a79df9dc72ea07543277331bae90fa809
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-28 19:37:27 +0100

    StringExample.java

---
+++ src/lesson4/code/lessoncode/StringExample.java
@@ -0,0 +1,10 @@
+package lesson4.code.lessoncode;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String myString = "Моя строка";
+
+        System.out.println(myString);
+
+    }
+}
