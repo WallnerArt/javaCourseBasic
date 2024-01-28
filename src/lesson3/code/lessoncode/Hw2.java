1c41dc981b3221ada2d7d5cf4ade8caad82269c2
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-28 19:16:53 +0100

    Hw2.java

---
+++ src/lesson3/code/lessoncode/Hw2.java
@@ -0,0 +1,10 @@
+package lesson3.code.lessoncode;
+
+public class Hw2 {
+    public static void main(String[] args) {
+        char charVariable = 'G';
+
+        System.out.println("переменная типа char: " + charVariable);
+    }
+}
+
