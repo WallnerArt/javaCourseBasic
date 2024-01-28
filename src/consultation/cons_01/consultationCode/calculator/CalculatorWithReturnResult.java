34e1c5a18a59f8b70a40c46d3e6dd8d4ad5338c1
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-28 19:04:24 +0100

    CalculatorWithReturnResult.java

---
+++ src/consultation/cons_01/consultationCode/calculator/CalculatorWithReturnResult.java
@@ -0,0 +1,27 @@
+package consultation.cons_01.consultationCode.calculator;
+
+public class CalculatorWithReturnResult {
+    public int sum(int a, int b){
+        int result = a + b;
+        return result;
+    }
+
+// добавить три метода которые бы возвращали результат вычитания, умножения и деления
+    // и использовать их
+
+
+    public int sub(int a, int b){
+        int result = a - b;
+        return result;
+    }
+
+    public int multiplication(int a, int b){
+        int result = a * b;
+        return result;
+    }
+
+    public double div(int a, int b){
+        double result = (double) a / b;
+        return result;
+    }
+}
