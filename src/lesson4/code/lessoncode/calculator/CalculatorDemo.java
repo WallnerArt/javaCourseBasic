0649ce0df186e719253ef5abf180e17c73e28846
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-28 19:02:55 +0100

    CalculatorDemo.java

---
+++ src/lesson4/code/lessoncode/calculator/CalculatorDemo.java
@@ -0,0 +1,15 @@
+package lesson4.code.lessoncode.calculator;
+
+public class CalculatorDemo {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int a = 10;
+        int b = 25;
+        calculator.sum(a, b);
+        calculator.multiplication(a, b);
+
+        calculator.sum(10,55);
+    }
+}
+
