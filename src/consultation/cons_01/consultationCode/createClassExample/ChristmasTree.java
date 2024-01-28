f82b0cd5c773a1cbad483f05492b67246753aa80
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-28 19:07:20 +0100

    ChristmasTree.java

---
+++ src/consultation/cons_01/consultationCode/createClassExample/ChristmasTree.java
@@ -0,0 +1,14 @@
+package consultation.cons_01.consultationCode.createClassExample;
+
+public class ChristmasTree {
+    String christmasTreeToys;
+
+    public ChristmasTree(String christmasTreeToys) {
+        this.christmasTreeToys = christmasTreeToys;
+    }
+
+    public void garlandSwitchOn(){
+        System.out.println("Гирлянда горит!");
+        System.out.println("Игрушки на елке:" + christmasTreeToys);
+    }
+}
