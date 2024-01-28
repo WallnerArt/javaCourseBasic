88759ea35322e3cc66f56bf358bfa4336a98d670
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-28 19:07:48 +0100

    ChristmasTreeDemo.java

---
+++ src/consultation/cons_01/consultationCode/createClassExample/ChristmasTreeDemo.java
@@ -0,0 +1,11 @@
+package consultation.cons_01.consultationCode.createClassExample;
+
+public class ChristmasTreeDemo {
+    public static void main(String[] args) {
+
+        ChristmasTree myChristmasTree = new ChristmasTree("Новогодние шарики");
+
+        myChristmasTree.garlandSwitchOn();
+
+    }
+}
