46ff7ec45760c672d44a8b7bd96c109daaf143c8
Eugen Wallner <156695437+WallnerArt@users.noreply.github.com>
2024-01-28 19:16:33 +0100

    Horse.java

---
+++ src/consultation/cons_01/consultationCode/zoo/Horse.java
@@ -0,0 +1,14 @@
+package consultation.cons_01.consultationCode.zoo;
+
+public class Horse {
+    String name;
+
+    public Horse(String name) {
+        this.name = name;
+    }
+
+    public void greetings(){
+        System.out.println("И-го-го!");
+    }
+
+}
