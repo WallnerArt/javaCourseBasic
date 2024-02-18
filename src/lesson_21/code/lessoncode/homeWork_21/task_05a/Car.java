a3561eeb4d5c631207dd54ed9e174ae19a0ebe51
WallnerArt <eugenwallner.design@gmail.com>
2024-02-18 22:57:10 +0100

    task_05a.java

---
+++ src/lesson_21/code/lessoncode/homeWork_21/task_05a/Car.java
@@ -0,0 +1,10 @@
+package lesson_21.code.lessoncode.homeWork_21.task_05a;
+
+class Car extends Vehicle {
+    int numberOfDoors;
+
+    // Специфический метод для автомобиля
+    void honk() {
+        System.out.println("Автомобиль сигналит.");
+    }
+}
---
+++ src/lesson_21/code/lessoncode/homeWork_21/task_05a/Vehicle.java
@@ -0,0 +1,12 @@
+package lesson_21.code.lessoncode.homeWork_21.task_05a;
+class Vehicle {
+    // Общие свойства для всех транспортных средств
+    int speed;
+    String color;
+
+    // Общий метод для всех транспортных средств
+    void move() {
+        System.out.println("Транспортное средство движется.");
+    }
+}
+
