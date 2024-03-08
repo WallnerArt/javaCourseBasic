9fc48192603e5d9ac29e3ba54d7301a0b17e926c
WallnerArt <eugenwallner.design@gmail.com>
2024-03-08 12:36:01 +0100

    homeWork.java

--- src/lesson_38/code/lessoncode/task_01/Main.java
+++ src/lesson_38/code/lessoncode/task_01/Main.java
@@ -6,10 +6,10 @@ public class Main {
         // Создаем объект Car
         Car car = new Car(4); // Создаем автомобиль с четырьмя дверьми
         // Создаем объекты дверей
-        Car.Door frontLeftDoor = car.new Door(1.2, 1.8); // Создаем левую переднюю дверь
-        Car.Door frontRightDoor = car.new Door(1.2, 1.8); // Создаем правую переднюю дверь
-        Car.Door rearLeftDoor = car.new Door(1.2, 1.8); // Создаем левую заднюю дверь
-        Car.Door rearRightDoor = car.new Door(1.2, 1.8); // Создаем правую заднюю дверь
+        Car.Door frontLeftDoor = car.new Door(200, 100); // Создаем левую переднюю дверь
+        Car.Door frontRightDoor = car.new Door(200, 100); // Создаем правую переднюю дверь
+        Car.Door rearLeftDoor = car.new Door(200, 100); // Создаем левую заднюю дверь
+        Car.Door rearRightDoor = car.new Door(200, 100); // Создаем правую заднюю дверь
 
         // Открываем и закрываем двери
         frontLeftDoor.open();
